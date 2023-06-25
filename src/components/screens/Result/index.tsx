b3aecf100e983375033086321a9a7554d2db4acd
LeDucLoi193 <dekaito193@gmail.com>
2023-06-25 23:53:23 +0700

    chore: separate mapping files

--- src/components/screens/Result/index.tsx
+++ src/components/screens/Result/index.tsx
@@ -5,37 +5,42 @@ import { Button } from 'flowbite-react'
 import pdfMake from 'pdfmake/build/pdfmake'
 import pdfFonts from 'pdfmake/build/vfs_fonts'
 import { IResult } from '@models/interface'
-import { MAPPING_IMAGE_BASE64 } from '@utils/encoding'
+import { MAPPING_TEMPLATE_IMAGE_BASE64 } from '@models/mapping/template'
+import { MAPPING_LIFE_PATH_IMAGE_BASE64 } from '@models/mapping/lifePath'
+import { MAPPING_SOUL_IMAGE_BASE64 } from '@models/mapping/soul'
+import { MAPPING_PERSONALITY_IMAGE_BASE64 } from '@models/mapping/personality'
+import { MAPPING_TALENT_IMAGE_BASE64 } from '@models/mapping/talent'
+import { MAPPING_PASSION_IMAGE_BASE64 } from '@models/mapping/passion'
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs
 
 const DEFAULT_CONTENT = [
   {
-    image: MAPPING_IMAGE_BASE64.get('/result/template/0a.png'),
+    image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/0a.png'),
     alt: 'Lời giới thiệu',
     width: 525,
     height: 750,
   },
   {
-    image: MAPPING_IMAGE_BASE64.get('/result/template/0b.png'),
+    image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/0b.png'),
     alt: 'Giới thiệu công ty',
     width: 525,
     height: 750,
   },
   {
-    image: MAPPING_IMAGE_BASE64.get('/result/template/0c.png'),
+    image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/0c.png'),
     alt: 'Giới thiệu công ty',
     width: 525,
     height: 750,
   },
   {
-    image: MAPPING_IMAGE_BASE64.get('/result/template/0d.png'),
+    image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/0d.png'),
     alt: 'Mục lục',
     width: 525,
     height: 750,
   },
   {
-    image: MAPPING_IMAGE_BASE64.get('/result/template/1.png'),
+    image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/1.png'),
     alt: 'Chân dung khách hàng',
     width: 525,
     height: 750,
@@ -100,7 +105,7 @@ export const Result: React.FC<IResult> = ({ lifePath, soul, personality, talent,
   for (const dataObject of data) {
     if (dataObject.type === CONTENT_LABEL.LIFE_PATH) {
       const newLifePathImages = dataObject.values.map((value) => ({
-        image: MAPPING_IMAGE_BASE64.get(value),
+        image: MAPPING_LIFE_PATH_IMAGE_BASE64.get(value),
         alt: 'Chân dung khách hàng',
         width: 525,
         height: 750,
@@ -108,14 +113,14 @@ export const Result: React.FC<IResult> = ({ lifePath, soul, personality, talent,
       content.push(...newLifePathImages)
     } else if (dataObject.type === CONTENT_LABEL.SOUL) {
       const newSoulImages = dataObject.values.map((value) => ({
-        image: MAPPING_IMAGE_BASE64.get(value),
+        image: MAPPING_SOUL_IMAGE_BASE64.get(value),
         alt: 'Tứ huyệt cảm xúc',
         width: 525,
         height: 750,
       }))
       content.push(
         {
-          image: MAPPING_IMAGE_BASE64.get('/result/template/2.png'),
+          image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/2.png'),
           alt: 'Tứ huyệt cảm xúc',
           width: 525,
           height: 750,
@@ -124,14 +129,14 @@ export const Result: React.FC<IResult> = ({ lifePath, soul, personality, talent,
       )
     } else if (dataObject.type === CONTENT_LABEL.PERSONALITY) {
       const newPersonalityImages = dataObject.values.map((value) => ({
-        image: MAPPING_IMAGE_BASE64.get(value),
+        image: MAPPING_PERSONALITY_IMAGE_BASE64.get(value),
         alt: 'Thiết lập mối quan hệ với khách hàng',
         width: 525,
         height: 750,
       }))
       content.push(
         {
-          image: MAPPING_IMAGE_BASE64.get('/result/template/3.png'),
+          image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/3.png'),
           alt: 'Thiết lập mối quan hệ với khách hàng',
           width: 525,
           height: 750,
@@ -140,14 +145,14 @@ export const Result: React.FC<IResult> = ({ lifePath, soul, personality, talent,
       )
     } else if (dataObject.type === CONTENT_LABEL.TALENT) {
       const newTalentImages = dataObject.values.map((value) => ({
-        image: MAPPING_IMAGE_BASE64.get(value),
+        image: MAPPING_TALENT_IMAGE_BASE64.get(value),
         alt: 'Chăm sóc khách hàng',
         width: 525,
         height: 750,
       }))
       content.push(
         {
-          image: MAPPING_IMAGE_BASE64.get('/result/template/4.png'),
+          image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/4.png'),
           alt: 'Chăm sóc khách hàng',
           width: 525,
           height: 750,
@@ -156,21 +161,21 @@ export const Result: React.FC<IResult> = ({ lifePath, soul, personality, talent,
       )
     } else {
       const newPassionImages = dataObject.values.map((value) => ({
-        image: MAPPING_IMAGE_BASE64.get(value),
+        image: MAPPING_PASSION_IMAGE_BASE64.get(value),
         alt: 'Đam mê của khách hàng',
         width: 525,
         height: 750,
       }))
       content.push(
         {
-          image: MAPPING_IMAGE_BASE64.get('/result/template/5.png'),
+          image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/5.png'),
           alt: 'Đam mê của khách hàng',
           width: 525,
           height: 750,
         },
         ...newPassionImages,
         {
-          image: MAPPING_IMAGE_BASE64.get('/result/template/6.png'),
+          image: MAPPING_TEMPLATE_IMAGE_BASE64.get('/result/template/6.png'),
           alt: 'Kết thúc',
           width: 525,
           height: 750,
